23c712bde2933d7bdab56c7f52f1473e4ca7321b
Tomomi Kajita <t-kajita@biglobe.co.jp>
2015-11-26 21:01:29 +0900

    属性のパース処理をOutputDataに移動
    Signed-off-by: Tomomi Kajita <t-kajita@biglobe.co.jp>

--- src/main/java/jp/co/biglobe/isp/monitor/OutputData.java
+++ src/main/java/jp/co/biglobe/isp/monitor/OutputData.java
@@ -1,9 +1,12 @@
 package jp.co.biglobe.isp.monitor;
 
+import javax.management.Attribute;
+import javax.management.AttributeList;
+import javax.management.ObjectName;
+import javax.management.openmbean.CompositeDataSupport;
+import javax.management.openmbean.CompositeType;
 import java.time.Instant;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -25,15 +28,87 @@ public class OutputData {
         Map<String,Object> map = new LinkedHashMap<>();
         map.put(SAMPLING_TIME_KEY, samplingTime);
         map.put(HOSTNAME_KEY, hostname);
-        map.putAll(samplingData.asMap());
+        map.putAll(parseSamplingData(samplingData));
 
         this.outputData = Collections.unmodifiableMap(map);
     }
 
+    private Map<String,Object> parseSamplingData(SamplingData samplingData) {
+
+        Map<String,Object> map = parseObjectName(samplingData.objectName);
+        map.putAll(parseAttributes(samplingData.attributes));
+
+        return map;
+    }
+
+    /**
+     * ObjectName を解析
+     * @param objectName
+     * @return
+     */
+    private Map<String,Object> parseObjectName(ObjectName objectName) {
+
+        Map<String,Object> map = new LinkedHashMap<>();
+        map.put(OBJECT_NAME_KEY, objectName.getCanonicalName());
+        map.put(DOMAIN_KEY, objectName.getDomain());
+        map.putAll(objectName.getKeyPropertyList());
+
+        return map;
+    }
+
+    /**
+     * AttributeList を解析
+     * @param attributes
+     * @return
+     */
+    private Map<String,Object> parseAttributes(AttributeList attributes) {
+        Map<String,Object> map = new LinkedHashMap<>();
+
+        for(Attribute attribute : attributes.asList()) {
+            if(attribute.getValue() instanceof CompositeDataSupport) {
+                map.putAll(parseCompositeDataSupport((CompositeDataSupport) attribute.getValue()));
+            } else {
+                map.put(attribute.getName(), attribute.getValue());
+            }
+        }
+
+        return map;
+    }
+
+    /**
+     * CompositeDataSupport を解析
+     * @param compositeDataSupport
+     * @return
+     */
+    private Map<String,Object> parseCompositeDataSupport(CompositeDataSupport compositeDataSupport) {
+
+        CompositeType compositeType = compositeDataSupport.getCompositeType();
+
+        Map<String,Object> map = new LinkedHashMap<>();
+        for (String key: compositeType.keySet()) {
+            Object value = compositeDataSupport.get(key);
+
+            if (!(value instanceof CompositeDataSupport)) {
+                map.put(key, value);
+            } else {
+                map.putAll(parseCompositeDataSupport((CompositeDataSupport) value));
+            }
+        }
+
+        return map;
+    }
 
     public String printLTSV() {
         return outputData.entrySet().stream()
+                .map(this::formatDateTime)
                 .map(entry -> String.format("%s:%s", entry.getKey(), entry.getValue()))
                 .collect(Collectors.joining("\t"));
+    }
+
+    private Map.Entry<String,Object> formatDateTime(Map.Entry<String,Object> source) {
+        if (SAMPLING_TIME_KEY.equals(source.getKey())) {
+            return new AbstractMap.SimpleImmutableEntry<>(SAMPLING_TIME_KEY, ((Instant)source.getValue()).toEpochMilli());
+        }
+        return source;
     }
 }
--- src/main/java/jp/co/biglobe/isp/monitor/SamplingData.java
+++ src/main/java/jp/co/biglobe/isp/monitor/SamplingData.java
@@ -6,12 +6,6 @@ import lombok.ToString;
 import javax.management.Attribute;
 import javax.management.AttributeList;
 import javax.management.ObjectName;
-import javax.management.openmbean.CompositeDataSupport;
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.stream.Collector;
 
 /**
  * 抽出データ
@@ -35,42 +29,5 @@ public class SamplingData {
         this.objectName = objectName;
         this.attributes = new AttributeList();
         attributes.add(new Attribute("exception", e));
-    }
-
-
-    public Map<String,Object> asMap() {
-        Map<String,Object> map = new LinkedHashMap<>();
-        map.put(OutputData.OBJECT_NAME_KEY, objectName.getCanonicalName());
-        map.put(OutputData.DOMAIN_KEY, objectName.getDomain());
-        map.putAll(objectName.getKeyPropertyList());
-
-        for(Attribute attribute : attributes.asList()) {
-            if(attribute.getValue() instanceof  CompositeDataSupport) {
-                map.putAll(parseCompositeDataSupport((CompositeDataSupport) attribute.getValue()));
-            } else {
-                map.put(attribute.getName(), attribute.getValue());
-            }
-        }
-
-        return map;
-    }
-
-    /**
-     * CompositeDataSupport を解析
-     * @param compositeDataSupport
-     * @return
-     */
-    public Map<String,Object> parseCompositeDataSupport(CompositeDataSupport compositeDataSupport) {
-        return compositeDataSupport.getCompositeType().keySet().stream()
-                .map(key -> new AbstractMap.SimpleImmutableEntry(key, compositeDataSupport.get(key)))
-                .collect(Collector.<Map.Entry, Map>of(
-                        () -> new HashMap<>(),
-                        (map, entry) -> map.put(entry.getKey(), entry.getValue()),
-                        (map01, map02) -> {
-                            map01.putAll(map02);
-                            return map01;
-                        },
-                        Collector.Characteristics.CONCURRENT
-                ));
     }
 }
